3467e2a129da312619811deb0ff764aed6d1d7aa
Satyam <satyam19arya@gmail.com>
2023-12-14 23:55:53 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+// import { useEffect, useState } from "react";
+// import TravelApp from "./components/TravelApp";
+// import { toast } from 'react-toastify';
 import FormValidation from "./components/FormValidation";
-import TravelApp from "./components/TravelApp";
 
 function App() {
+  // const [courses, setCourses] = useState([]);
+  // const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  // async function fetchData() {
+  //   setLoading(true);
+  //   try{
+  //     const response = await fetch(apiurl);
+  //     const data = await response.json();
+  //     setCourses(data);
+  //   } catch(error){
+  //     toast.error("Network error");
+  //   }
+  //   setLoading(false);
+  // }
+
   return (
     <div>
-      <TravelApp />
-      {/* <FormValidation /> */}
+      {/* <TravelApp /> */}
+      <FormValidation />
+      {/* <div>
+        {loading ? (<Spinner />) : (<Cards courses={courses}/>)}
+      </div> */}
     </div>
   );
 }
